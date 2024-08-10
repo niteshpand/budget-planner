9d2981b36e45423b1b6c17e3f5b4faa817f891cc
nitesh <pandeynitesh822@gmail.com>
2024-08-10 23:27:18 +0530

    create side nav component

---
+++ src/app/budget-planner/side-nav/side-nav.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { Router } from '@angular/router';
+@Component({
+  selector: 'app-side-nav',
+  standalone: true,
+  imports: [MatIconModule],
+  templateUrl: './side-nav.component.html',
+  styleUrl: './side-nav.component.css',
+})
+export class SideNavComponent {
+  isSlideOut = true;
+  constructor(private route: Router) {}
+
+  toggleSlideOut() {
+    this.isSlideOut = !this.isSlideOut;
+  }
+  onDash() {}
+  onProfile() {}
+  onHistory() {}
+  onLogout() {}
+}
